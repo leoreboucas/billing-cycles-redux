cbe1b15e67dc7e0c4d23973770c224df44fe07f4
Leonardo Rebouças <leoabcblm@gmail.com>
2022-10-06 23:45:04 -0300

    frontend configurado

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
 import '../common/template/dependencies'
-
+import React from 'react';
 
 function App() {
-
   return (
-    <div className="App">
-      Olá
+    <div className='wrapper'>
+        <h1>App</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
